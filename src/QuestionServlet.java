192a89f5c70e93b3830aca90392699c5cdbc1d68
AlexandrBerdar <officialofberdar@gmail.com>
2018-04-18 19:03:00 +0300

    k

--- src/QuestionServlet.java
+++ src/QuestionServlet.java
@@ -1,7 +1,10 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+@WebServlet("/question")
 
 public class QuestionServlet extends HttpServlet {
 
